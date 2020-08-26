954f8a07d8d42fac588c81bbf589448c805c828e
lubisarieff <lubisarieff@gmail.com>
2020-08-26 08:48:10 +0700

    Change Type ObjectId to string Id

--- MongoRepository/Core/Entities/Entity.cs
+++ MongoRepository/Core/Entities/Entity.cs
@@ -1,7 +1,12 @@
-﻿namespace MongoRepository.Core.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoRepository.Core.Entities
 {
     public abstract class Entity : IEntity
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
     }
 }
--- MongoRepository/Core/Entities/IEntity.cs
+++ MongoRepository/Core/Entities/IEntity.cs
@@ -1,11 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoRepository.Core.Entities
 {
     public interface IEntity
     {
         [BsonId]
-        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonRepresentation(BsonType.ObjectId)]
         string Id { get; set; }
     }
 }
